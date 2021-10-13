7b4150396b3754d72c747022f5fd460e0c9d98ce
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-13 19:49:13 +0100

    Installed django restframework

--- backendbooks/base/views.py
+++ backendbooks/base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import APIView, api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
+@api_view(['GET'])
 def testApp(request):
-    return JsonResponse('Hello world', safe=False)
+    return Response('Hello world') 
