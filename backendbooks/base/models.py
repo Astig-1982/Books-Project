51a1c0d89d1fe25332e73e55c4b2da153b772b45
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-13 21:25:43 +0100

    Finished creating the models

--- backendbooks/base/models.py
+++ backendbooks/base/models.py
@@ -1,29 +1,66 @@
 from django.db import models
-from django.db.models.fields import BooleanField, DecimalField, IntegerField
+from django.db.models.fields import BooleanField, DecimalField, IntegerField, TextField
 from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 
-class Genre(models.Model):
+class Book_Genre(models.Model):
     name = models.CharField(max_length=254, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Books(models.Model):
     name = models.CharField(max_length=254)
     # image = models.ImageField(null=True, blank=True)
-    genre = ForeignKey('Genre', on_delete=models.SET_NULL, max_length=50, null=False, blank=True) #blank=True means the filed is required to be completed in django admin 
-    author = ForeignKey('Author', on_delete=models.SET_NULL, max_length=254, null=False, blank=True)
-    period = ForeignKey('Period', on_delete=models.SET_NULL, max_length=100, null=False, blank=True)
-    year_of_writting = DecimalField(max_digits=4, null=True, blank=False)
-    number_of_pages = DecimalField(null=False, blank=True)
+    genre = ForeignKey('Book_Genre', on_delete=models.SET_NULL, max_length=50, null=True, blank=True) # blank=True means the filed is required to be completed in django admin 
+    author = ForeignKey('Author', on_delete=models.SET_NULL, max_length=254, null=True, blank=True)
+    about = TextField(max_length=300, null=True, blank=False)
+    summary = TextField(null=True, blank=False)
+    period = ForeignKey('Period', on_delete=models.SET_NULL, max_length=100, null=True, blank=True)
+    year_of_writting = IntegerField(null=True, blank=False)
+    number_of_pages = IntegerField(null=False, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Author(models.Model):
     name = models.CharField(max_length=254, null=True, blank=False)
+    # image = models.ImageField(null=True, blank=True)
     sex = models.CharField(max_length=254, null=True, blank=False)
     nationality = ForeignKey('Nationality', on_delete=models.SET_NULL, null=True, blank=True)
     period = ForeignKey('Period', on_delete=models.SET_NULL, null=True, blank=True)
     still_alive = BooleanField(default=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Nationality(models.Model):
+    class Meta:
+        verbose_name_plural = 'Nationalities' # this will show 'Natinoalities' in django admin
+
+    name = models.CharField(max_length=254, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Period(models.Model):
+    name = models.CharField(max_length=254, null=True, blank=False)
+    time_frames = models.CharField(max_length=50, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Styles(models.Model):
+    name = models.CharField(max_length=254, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
 
 
 
