62a85828fb87b25d48ccf70bce9188bf52b549e2
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-14 19:59:17 +0100

    Created homepage component

---
+++ frontendbooks/frontendbooks/src/pages/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homepage.styles.css';
+
+const HomePage = () => {
+    return(
+        <div className='home-page'>
+            hello world
+        </div>
+    );
+};
+
+export default HomePage;
