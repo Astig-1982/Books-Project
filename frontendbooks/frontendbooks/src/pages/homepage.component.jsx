5cfe73975eb58312ba864156dc666010d2e73a83
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-16 20:10:05 +0100

    Created getAllBook async function to make the call to the api

--- frontendbooks/frontendbooks/src/pages/homepage.component.jsx
+++ frontendbooks/frontendbooks/src/pages/homepage.component.jsx
@@ -1,20 +1,34 @@
 import React, { useState, useEffect} from 'react';
 
-import './homepage.styles.css';
-
-
-
 const HomePage = () => {
 
+    const [books, getBooks] = useState()
+    const [error, setError] = useState()
+
     useEffect(() => {
-        console.log("I am using useEffect")
+        const getAllBooks = async() => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/books/');
+                const books = await response.json()
+                getBooks(books)
+                }
+            catch(error) {
+                setError(error)
+                }
+        }
+
+        getAllBooks()
     }, [])
 
+    console.log({error})
+    console.log({books})
     return(
         <div className='home-page'>
             <h1>The Books Universe</h1>
         </div>
+        
     );
 };
 
 export default HomePage;
+
