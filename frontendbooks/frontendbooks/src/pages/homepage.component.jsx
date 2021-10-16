65523e4ca17eed3bc709380c3212ae8100c8afba
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-16 19:00:52 +0100

    Added comments

--- frontendbooks/frontendbooks/src/pages/homepage.component.jsx
+++ frontendbooks/frontendbooks/src/pages/homepage.component.jsx
@@ -2,7 +2,14 @@ import React, { useState, useEffect} from 'react';
 
 import './homepage.styles.css';
 
+
+
 const HomePage = () => {
+
+    useEffect(() => {
+        console.log("I am using useEffect")
+    }, [])
+
     return(
         <div className='home-page'>
             <h1>The Books Universe</h1>
