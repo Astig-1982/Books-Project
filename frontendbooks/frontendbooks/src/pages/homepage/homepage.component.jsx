da7c23513d06c96554886c909d6c4792ec5e945c
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-18 21:46:04 +0100

    Added bootstrap and started styling HomePage component with bootstrap elements

--- frontendbooks/frontendbooks/src/pages/homepage/homepage.component.jsx
+++ frontendbooks/frontendbooks/src/pages/homepage/homepage.component.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import HomeSection from '../../components/homesection.component';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const HomePage = () => {
 
     return(
         <div>
-            <Link to='/all-books'>
-                <HomeSection sectionLink={'log in'} />
-            </Link>
-            <Link to='/all-books'>
-                <HomeSection sectionLink={'books'} />
-            </Link>
-            <Link to='/all-books'>
-                <HomeSection sectionLink={'authors'} />
-            </Link>
+            <Container >
+                <Row>
+                    <Col>
+                        <Link to='/all-books'>
+                            <HomeSection sectionLink={'log in'} />
+                        </Link>
+                    </Col>
+                    <Col>
+                        <Link to='/all-books'>
+                            <HomeSection sectionLink={'books'} />
+                        </Link>
+                    </Col>
+                    <Col>
+                        <Link to='/all-books'>
+                            <HomeSection sectionLink={'authors'} />
+                        </Link>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
     
