251f8a261bc0af79fb4bdb9379550a4072fb01ff
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-24 12:09:59 +0100

    Added call to the API

--- frontendbooks/frontendbooks/src/components/bookdetailed/bookdetailed.component.jsx
+++ frontendbooks/frontendbooks/src/components/bookdetailed/bookdetailed.component.jsx
@@ -1,16 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import './bookdetailed.styles.css';
 
-const BookDetailed = () => {
+const BookDetailed = ({match}) => {
 
     const [detailedBook, setDetailedBook] = useState('book in detailed')
+    const [error, setError] = useState('')
 
+    useEffect(() => {
+        // this function calls the API and retrieves the book in detailed
+        const getAllBooks = async() => {
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/all_books/book_detailed/${match.params.book_id}`)
+                const book = await response.json()
+                setDetailedBook(book)
+                }
+            catch(error) {
+                setError('there is an error')
+                }
+        }
+
+        getAllBooks()
+    }, [])
+
+    console.log(detailedBook)
+    console.log(error)
     return(
         <div className='book-detailed'>
-            hello book detailed
-            <h1>{detailedBook}</h1>
+            <h1>{error}</h1>
+            <h1>{detailedBook.name}</h1>
         </div>
     );   
 }
 
 export default BookDetailed
+
+// props.match.params.id
