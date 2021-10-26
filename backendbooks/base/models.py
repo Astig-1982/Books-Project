da5ee50d7364d7e1f0a4bfae132408e2d7c26b89
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-26 20:51:02 +0100

    Started to add items and images to the database

--- backendbooks/base/models.py
+++ backendbooks/base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.fields import IntegerField, TextField
+from django.db.models.fields import IntegerField, TextField, BooleanField
 from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
@@ -17,7 +17,10 @@ class Book_Genre(models.Model):
 class Book(models.Model):
     name = models.CharField(max_length=254)
     image = models.ImageField(null=True, blank=True)
-    genre = ForeignKey(Book_Genre, on_delete=models.SET_NULL, max_length=50, null=True, blank=True) # blank=True means the filed is required to be completed in django admin 
+    multiple_genre = BooleanField(default=True, null=True, blank=True)
+    genre1 = ForeignKey(Book_Genre, on_delete=models.SET_NULL, max_length=50, null=True, blank=False, related_name='main_genre') # blank=True means the filed is required to be completed in django admin
+    genre2 = ForeignKey(Book_Genre, on_delete=models.SET_NULL, max_length=50, null=True, blank=True, related_name='secondary_genre')
+    genre3 = ForeignKey(Book_Genre, on_delete=models.SET_NULL, max_length=50, null=True, blank=True, related_name='third_genre') 
     author = ForeignKey(Author, on_delete=models.SET_NULL, max_length=254, null=True, blank=True)
     about = TextField(max_length=300, null=True, blank=False)
     summary = TextField(null=True, blank=False)
