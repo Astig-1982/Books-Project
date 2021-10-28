6e0cb0452ac641fed896d8dedc4a9c6ba534b66f
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-28 19:41:59 +0100

    Deleted url for all_books_a_to_z view

--- backendbooks/base/urls.py
+++ backendbooks/base/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.books_urls, name='books_urls'),
     path('all_books/<order_by>', views.all_books, name='all_books/default_order'),
-    # path('all_books/a_to_z', views.all_books_a_to_z, name='aall_books_a_to_z'),
     path('all_books/book_detailed/<book_id>', views.book_detailed, name='book-detailed')
 ]
