fe774ce19bf2c2db09c802fba1be4db9758ee900
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-31 20:17:16 +0000

    Added StringRelatedField to BookSerializer

--- backendbooks/base/serializers.py
+++ backendbooks/base/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
 from .models import Book, Style
+from authors.models import Author
 
 class BookSerializer(serializers.ModelSerializer):
     """
     this serializer is for the Book model
     """
+    author = serializers.StringRelatedField(many=False)
+    
     class Meta:
         model = Book
         fields = '__all__'
