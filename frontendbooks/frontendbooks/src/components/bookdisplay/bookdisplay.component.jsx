ecb8e595a6cdca17dbb4d5fd8c0c26f2bdd0aafc
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-10-31 20:15:23 +0000

    Addded Card component from bootstrap

--- frontendbooks/frontendbooks/src/components/bookdisplay/bookdisplay.component.jsx
+++ frontendbooks/frontendbooks/src/components/bookdisplay/bookdisplay.component.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap'
 
 import './bookdisplay.styles.css';
 
 const BookDisplay = ({ book }) => {
+    
     return(
+        
         <div className='book-div'>
-            <Link to={`/all-books/book-detailed/${book.id}`}>
+                <Card className='book-card' style={{ width: '10rem' }}>
+                    <Link to={`/all-books/book-detailed/${book.id}`}>
+                        <Card.Img className='book-image' variant="top" src={book.image} />
+                    </Link>
+                    <Card.Body>
+                        <Card.Title>{book.name}</Card.Title>
+                        <Card.Text>
+                            {book.author}
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>  
+        </div>
+    );   
+}
+
+export default BookDisplay
+
+
+
+/*
+<Link to={`/all-books/book-detailed/${book.id}`}>
                 <div className='link-div'>
                     <img src={book.image} className='book-image' />
                     <div className='book-details'>
@@ -14,8 +38,4 @@ const BookDisplay = ({ book }) => {
                     </div>
                 </div> 
             </Link>
-        </div>
-    );   
-}
-
-export default BookDisplay
+*/
