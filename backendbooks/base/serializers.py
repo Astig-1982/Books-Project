a64a1de1936cded225711f58405d2030c45bd49e
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-11-10 22:06:52 +0000

    Added depth to BookSerializer

--- backendbooks/base/serializers.py
+++ backendbooks/base/serializers.py
@@ -7,16 +7,18 @@ class BookSerializer(serializers.ModelSerializer):
     """
     this serializer is for the Book model
     """
+    """
     author = serializers.SlugRelatedField(many=False, read_only='True', slug_field='name') # StringRelatedField is used to make the relation with Author model. It is used for author filed. Check StringRelatedField at django rest api documentation
     period = serializers.StringRelatedField(many=False)
     type = serializers.StringRelatedField(many=False)
     genre1 = serializers.StringRelatedField(many=False)
     genre2 = serializers.StringRelatedField(many=False)
     genre3 = serializers.StringRelatedField(many=False)
-    
+    """
     class Meta:
         model = Book
         fields = '__all__'
+        depth = 2
 
 
 
