013a4444a49b2ade5418e9fa7834b790fc8cb4a5
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-11-15 16:02:37 +0000

    Set up main colors

--- frontendbooks/frontendbooks/src/components/bookdisplay/bookdisplay.component.jsx
+++ frontendbooks/frontendbooks/src/components/bookdisplay/bookdisplay.component.jsx
@@ -13,13 +13,15 @@ const BookDisplay = ({ book }) => {
                     <Link to={`/all-books/book-detailed/${book.id}`}>
                         <Card.Img className='book-image' variant="top" src={book.image} />
                     </Link>
-                    <Card.Body>
+                    <Card.Body className='book-body'>
                         <Card.Title className='book-title'><h5 className={`${book.name.length >= 24 ? 'long-title' : null}`}>{book.name}</h5></Card.Title>
                         <Card.Text className='book-author'>
                             <div className='book-details-1'>{book.period.name} {book.type.name}</div>
                             <div> By {book.author.name}</div>
+                            <div className='book-details-2'>
                             <div className='book-genre'>{book.genre1.name} {book.genre2 ? book.genre2.name : null} {book.genre3 ? book.genre3.name : null}</div>
                             <div className='origins'>{book.author.nationality.name} Literature</div>
+                            </div>
                         </Card.Text>
                     </Card.Body>
                 </Card>  
