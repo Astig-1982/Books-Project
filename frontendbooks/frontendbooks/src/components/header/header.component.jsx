984687426d206f33481a82ba3b8469596b0df78e
Astig Mandalian <astig.mandalian2014@gmail.com>
2021-11-20 15:26:45 +0000

    Added links to navbar

--- frontendbooks/frontendbooks/src/components/header/header.component.jsx
+++ frontendbooks/frontendbooks/src/components/header/header.component.jsx
@@ -10,19 +10,24 @@ const Header = () => {
         <div>
             <Navbar className='navbar' expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand>
+                        <Link className='navbar-brand-link' to=''> 
+                            BOOKS CORNER
+                        </Link>   
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link className='books' href="#home">
+                            <Link to='/all-books/no_filter' className='links'>
+                                BOOKS
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link className='authors' href="#link">
+                            <Link to='/all-books/no_filter' className='links'>
+                                AUTHORS
+                            </Link>
+                        </Nav.Link>    
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
